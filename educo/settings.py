d7c58560971e5dda0332a21ff9e109635329a4c5
Konrad M <10982159+konmacie@users.noreply.github.com>
2023-03-23 23:12:01 +0100

    settings update

--- educo/settings.py
+++ educo/settings.py
@@ -11,10 +11,22 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import environ
+from corsheaders.defaults import default_headers
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+
+# Load environment variables
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, True),
+    SESSION_COOKIE_SECURE=(bool, False),
+    CSRF_COOKIE_SECURE=(bool, False),
+)
+
+environ.Env.read_env(BASE_DIR / 'environments' / '.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
