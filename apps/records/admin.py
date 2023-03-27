a1867c67313249b6664290e2ce2ddb3434b901c7
Konrad M <10982159+konmacie@users.noreply.github.com>
2023-03-27 22:31:52 +0200

    update Student and Teacher admin, add Profile

--- apps/records/admin.py
+++ apps/records/admin.py
@@ -20,18 +20,27 @@ class CustomUserAdmin(UserAdmin):
             },
         ),
     )
-    list_display = ("username", "email", "first_name",
-                    "last_name", "is_staff", "role")
 
-    list_filter = ("is_staff",
-                   "is_superuser", "is_active", "groups", "role")
+
+class TeacherAdmin(CustomUserAdmin):
+    list_display = ("username", "first_name",
+                    "last_name", "is_active")
+
+    list_filter = ("is_active", )
+
+
+class ProfileInline(admin.StackedInline):
+    model = models.Profile
+    can_delete = False
 
 
 class StudentAdmin(CustomUserAdmin):
-    list_display = ("username", "email", "first_name",
-                    "last_name", "current_group")
+    list_display = ("username", "first_name",
+                    "last_name", "current_group", "is_active")
 
-    list_filter = ("is_active", "groups", "role")
+    list_filter = ("is_active", )
+
+    inlines = [ProfileInline]
 
     def current_group(self, obj):
         return obj.current_group
@@ -51,20 +60,42 @@ class StudentGroupAdmin(admin.ModelAdmin):
         return super().get_queryset(request).with_students_count()
 
 
+class AssignmentTimeframeListFilter(admin.SimpleListFilter):
+    LOOKUP_CURRENT = 'current'
+    LOOKUP_PAST = 'past'
+    LOOKUP_FUTURE = 'future'
+
+    LOOKUP_CHOICES = (
+        (LOOKUP_CURRENT, _('Current')),
+        (LOOKUP_PAST, _('Past')),
+        (LOOKUP_FUTURE, _('Future')),
+    )
+
+    title = _('Timeframe')
+    parameter_name = 'Timeframe'
+
+    def lookups(self, request, model_admin):
+        return self.LOOKUP_CHOICES
+
+    def queryset(self, request, queryset):
+        if self.value() == self.LOOKUP_CURRENT:
+            return queryset.current()
+        elif self.value() == self.LOOKUP_PAST:
+            return queryset.past()
+        elif self.value() == self.LOOKUP_FUTURE:
+            return queryset.future()
+        else:
+            return queryset
+
+
 class StudentGroupAssignmentAdmin(admin.ModelAdmin):
     list_display = ('student', 'group', 'date_start', 'date_end')
+    list_filter = ('group', AssignmentTimeframeListFilter)
     search_fields = ('student__first_name',
                      'student__last_name', 'group__name')
 
 
-# class TeacherAdmin(CustomUserAdmin):
-
-#     def get_changeform_initial_data(self, request):
-#         initial = super().get_changeform_initial_data(request)
-#         initial.setdefault('is_teacher', True)
-#         return initial
-
-admin.site.register(models.Teacher, CustomUserAdmin)
+admin.site.register(models.Teacher, TeacherAdmin)
 admin.site.register(models.Student, StudentAdmin)
 admin.site.register(models.StudentGroup, StudentGroupAdmin)
 admin.site.register(models.StudentGroupAssignment, StudentGroupAssignmentAdmin)
